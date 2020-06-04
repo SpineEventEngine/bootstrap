9e5d12a3dc380474777c5d47cccdfeb98e3e42c5
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2020-06-04 21:11:42 +0300

    Advance version
    Signed-off-by: Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>

--- version.gradle.kts
+++ version.gradle.kts
@@ -32,4 +32,4 @@
 val spineBaseVersion: String by extra("1.5.12")
 val spineTimeVersion: String by extra("1.5.12")
 val spineVersion: String by extra("1.5.14")
-val pluginVersion: String by extra("1.5.15")
+val pluginVersion: String by extra("1.5.17")
