de678d54b4d032cf9a9e91dbabca868b6299f6f6
Alex Tymchenko <alex.tymchenko@teamdev.com>
2022-06-27 17:04:50 +0300

    Bump the Spine version to `1.8.2`.

--- version.gradle.kts
+++ version.gradle.kts
@@ -35,8 +35,8 @@
  * already in the root directory.
  */
 
-val spineVersion: String by extra("1.8.0")
-val spineBaseVersion: String by extra("1.8.0")
+val spineVersion: String by extra("1.8.2")
+val spineBaseVersion: String by extra("1.8.2")
 val spineTimeVersion: String by extra(spineVersion)
 val spineWebVersion: String by extra(spineVersion)
 val spineGCloudVersion: String by extra(spineVersion)
