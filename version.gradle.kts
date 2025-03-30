8670af59dd34c119c21c1d03fabdb402262003f0
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-03-30 20:52:28 +0100

    Bump version -> `2.0.0-SNAPSHOT.100`

--- version.gradle.kts
+++ version.gradle.kts
@@ -35,9 +35,10 @@
  * already in the root directory.
  */
 
-val spineVersion: String by extra("1.8.0")
-val spineBaseVersion: String by extra("1.8.0")
-val spineTimeVersion: String by extra(spineVersion)
-val spineWebVersion: String by extra(spineVersion)
-val spineGCloudVersion: String by extra(spineVersion)
-val pluginVersion: String by extra(spineVersion)
+val spineVersion: String by extra("2.0.0-SNAPSHOT.302")
+val spineBaseVersion: String by extra("2.0.0-SNAPSHOT.307")
+val spineTimeVersion: String by extra("2.0.0-SNAPSHOT.136")
+val spineWebVersion: String by extra("2.0.0-SNAPSHOT.74")
+val spineGCloudVersion: String by extra("2.0.0-SNAPSHOT.174")
+val pluginVersion: String by extra("2.0.0-SNAPSHOT.100")
+val bootstrapVersion: String by extra(pluginVersion)
